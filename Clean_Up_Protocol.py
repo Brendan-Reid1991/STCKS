a33c4374e3bc747704ed30dbff5ed8cea3f14e21
Brendan Reid <brendanreid@Jebediah.local>
2020-09-28 22:07:32 +0100

    more bugs

--- Clean_Up_Protocol.py
+++ Clean_Up_Protocol.py
@@ -8,3 +8,12 @@ stock_names = []
 for file in os.listdir('Watch/'):
     if file.endswith('.csv'):
         stock_names.append(file[0:-4])
+
+path = 'Watch/'
+
+for s in stock_names:
+    csv = pd.read_csv(path + s + '.csv', index_col=0, skip_blank_lines=True)
+    indices = list(np.where(csv['Date'].isnull())[0])
+    # csv.drop(csv.index[indices])
+    # print('    ',len(csv))
+    print(csv.index[indices])
